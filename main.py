cac83b472638a191fbc3fa1d0dd0e2af1297f703
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-27 21:49:26 +0800

    fix thing

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def prime(n):
   if n <= 1:
     ret = False
   else:
-    for i in range(2, (n ** 0.5) + 1):
+    for i in range(2, int(n ** 0.5) + 1):
       if n % i == 0:
         ret = False
         break
