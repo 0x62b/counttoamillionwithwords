e2b74c34071db2def2efa1ed6055a8b8dae5bca1
Benjamin Yang <benjamin.yang12@outlook.com>
2025-12-27 17:13:50 +0800

    flask server

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import os
+
+from dotenv import load_dotenv
+from flask import Flask
 from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
-from dotenv import load_dotenv
 
 load_dotenv()
 
 app = App(
-    token=os.getenv("SLACK_BOT_TOKEN"),
-    signing_secret=os.getenv("SLACK_SIGNING_SECRET")
+  token=os.getenv("SLACK_BOT_TOKEN"),
+  signing_secret=os.getenv("SLACK_SIGNING_SECRET")
 )
 
 CHANNEL = "C0A5RFXHXJN"
+
+@app.event("message.channels")
+def new_message(event, say):
+  pass
+
+flask = Flask(__name__)
+handler = SlackRequestHandler(app)
+
+if __name__ == "__main__":
+  flask.run(host="0.0.0.0", port=5000)
